5a23c00a765853a351da05b1e13a262056a677ec
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-01-26 22:17:53 +0530

    Time: 43 ms (86.48%), Space: 29.1 MB (50.27%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        if (nums.size() < 3)
-      return {};
-
-    vector<vector<int>> ans;
-
-    ranges::sort(nums);
-
-    for (int i = 0; i + 2 < nums.size(); ++i) {
-      if (i > 0 && nums[i] == nums[i - 1])
-        continue;
-     
-      int l = i + 1;
-      int r = nums.size() - 1;
-      while (l < r) {
-        const int sum = nums[i] + nums[l] + nums[r];
-        if (sum == 0) {
-          ans.push_back({nums[i], nums[l++], nums[r--]});
-          while (l < r && nums[l] == nums[l - 1])
-            ++l;
-          while (l < r && nums[r] == nums[r + 1])
-            --r;
-        } else if (sum < 0) {
-          ++l;
-        } else {
-          --r;
+        vector<vector<int>> result; 
+        int n = nums.size();
+        if(n < 3) {
+            return {}; 
         }
-      }
-    }
+        sort(nums.begin(), nums.end());
 
-    return ans;
+        for(int i = 0; i < n - 2; i++) {
+       
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int n1 = nums[i];
+            int target = -n1; 
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right) {
+                int sum = nums[left] + nums[right];
+                if(sum < target) {
+                    left++;
+                } else if(sum > target) {
+                    right--;
+                } else {
+                    result.push_back({n1, nums[left], nums[right]});
+
+                    
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
     }
 };
