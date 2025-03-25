41466df91d6bb6c69a2ed4560326f14c1078226b
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-03-25 13:57:32 +0530

    Time: 1 ms (14.51%), Space: 19.5 MB (64.34%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,20 +3,20 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size();
         int i = 0;
-        int j = n-1;
-// Two ppointer Algorithm
-        while(i<j){
-            if(numbers[i]+numbers[j] > target){
+        int j = n -1;
+
+        while(i < j){
+            if(numbers[i]+ numbers[j] > target){
                 j--;
             }
-            else if(numbers[i]+numbers[j] < target){
+            else if(numbers[i] + numbers[j] < target){
                 i++;
             }
             else{
-                return{i+1,j+1};           // 1 based indexing ke liye i+1; j+1
-            }
+                return{i+1, j+1};
+            } 
         }
-        return{};
-    }
 
+        return {};
+    }
 };
