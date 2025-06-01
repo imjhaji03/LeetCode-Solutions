f95083364dbfe7a0ef9c074915039b99cb9e8564
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-06-01 18:51:21 +0530

    Time: 0 ms (100%), Space: 10.1 MB (77.33%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,18 +2,56 @@ class Solution {
 public:
     string reverseWords(string s) {
         
-        stringstream ss(s);
+        // Approach using stringstream:
 
-        string token = " ";
-        string result = "";
+        // Use stringstream to split the input string s into individual words.
 
-        while(ss>>token){
-            result = token + " " + result;
+        // Store each word in reverse order by prepending it to a result string.
+
+        // After the loop, the result string will have an extra space at the end.
+
+        // Use substr(0, n - 1) to remove the trailing space and return the final result.
+
+        // stringstream ss(s);
+
+        // string token = " ";
+        // string result = "";
+
+        // while(ss>>token){
+        //     result = token + " " + result;
+        // }
+
+        // int n = result.size();
+
+        // return result.substr(0,n-1);
+
+
+        reverse(s.begin(),s.end());
+
+        int n = s.size();
+
+        int i = 0;
+        
+        int l = 0, r = 0;
+
+        while(i < n){
+            while(i < n && s[i] != ' '){
+                s[r] = s[i];
+                i++;
+                r++;
+            }
+            if(l<r){
+                reverse(s.begin()+l, s.begin()+r);
+                s[r] = ' ';
+                r++;
+
+                l = r;
+            }
+
+            i++;
         }
 
-        int n = result.size();
+        return s.substr(0, r-1);
 
-        return result.substr(0,n-1);
-        
     }
 };
