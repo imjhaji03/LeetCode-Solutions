ab50d9aa5cd760141ba1765a9ea01ebd643c08f0
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-06-07 23:14:38 +0530

    Time: 0 ms (100%), Space: 11.6 MB (75.19%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int count = 0;
+
+        // - Copies non-`val` elements forward in the array. Count becomes the new length after removal.
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] !=val){
                 nums[count] = nums[i];
