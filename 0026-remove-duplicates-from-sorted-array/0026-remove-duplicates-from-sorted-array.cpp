0184969608e5694d5320e47e2bf04891a4ca6a67
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-06-12 19:33:12 +0530

    Time: 1 ms (19.9%), Space: 22.5 MB (78.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicates(vector<int>& nums) { //Two pointer
         int n = nums.size();
 
-        int i = 0;
+        int i = 0; 
         for(int j = 1; j < n; j++){
             if(nums[i] != nums[j]){
-                i++;
+                i++; // pehele yaha per isko update kero
                 nums[i] = nums[j];
             }
         }
         return i+1;
-        
     }
 };
