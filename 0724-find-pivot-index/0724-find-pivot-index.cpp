dc6d61df436ef60ccdd02aa2012debce15190d0b
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-13 16:57:38 +0530

    Time: 0 ms (100%), Space: 37.1 MB (6.18%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -16,9 +16,30 @@ public:
         }
         return -1;
     }
+
+    int optimizedapproach(vector<int> & nums){
+        vector<int> lsum(nums.size(), 0);
+        vector<int> rsum(nums.size(), 0);
+        // calc the lsum
+        for(int i = 1; i < nums.size(); i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
+        }
+        // calc the rsum
+        for(int i = nums.size()-2; i >= 0; i--){
+            rsum[i] = rsum[i+1] + nums[i+1];
+        }
+
+        for(int i = 0; i < nums.size(); i++){ // using the interation from 1 -> n as it need leftmost pivot index
+            if(lsum[i] == rsum[i]){
+                return i;
+            }
+        }
+        return -1;
+
+    }
     int pivotIndex(vector<int>& nums) {
 
-        return bruteforce(nums);
+        return optimizedapproach(nums);
         
     }
 };
