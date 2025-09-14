691bbfe48372ee89a762dc59c063e1ad3e03ad8d
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-14 16:04:52 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
 
-    int sortingmethod(vector<int> & nums){
+    int xormethod(vector<int> &nums){
+        int ans = 0;
+
+        // Xor of all the elements
+        for(int i = 0; i < nums.size(); i++){
+            ans ^= nums[i];
+        }
+
+        // Xor of the range [0, N];
+        for(int i = 0; i <= nums.size(); i++){
+            ans ^= i;
+        }
+
+        return ans;
+    }
+
+    int sortingmethod(vector<int> &nums){
         sort(nums.begin(),nums.end());
         for(int i = 0; i < nums.size(); i++){
             if(i == nums[i]) continue;
@@ -12,7 +28,7 @@ public:
         return nums.size(); // corner case -> What if last element is missing.
     }
     int missingNumber(vector<int>& nums) {
-        return sortingmethod(nums);
+        return xormethod(nums);
         
     }
 };
