e811b4d73831d23e0759a2cc444b8559883f2d2f
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-14 15:35:19 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,38 +1,64 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int countzero = 0;
-        int countone = 0;
-        int counttwo = 0;
-        for ( int i = 0; i< nums.size(); i++) {
-            if ( nums[i] == 0) {
-                countzero++;
+        // using stl TC O(nlongn) SC O(n)
+        // sort(nums.begin(), nums.end());
+
+        // using countmethod TC O(n) SC O(1)
+        // here we are overwriting it so this is not a inplace sorting
+        // int zerocount = 0;
+        // int onecount = 0;
+        // int twocount = 0;
+
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(nums[i] == 0){
+        //         zerocount++;
+        //     }
+        //     else if(nums[i] == 1){
+        //         onecount++;
+        //     }
+        //     else{
+        //         twocount++;
+        //     }
+        // }
+
+        // int index = 0;
+        // while(zerocount--){
+        //     nums[index] = 0;
+        //     index++;
+        // }
+        // while(onecount--){
+        //     nums[index] = 1;
+        //     index++;
+        // }
+        // while(twocount--){
+        //     nums[index] = 2;
+        //     index++;
+        // }
+
+        // Inplace Sorting
+
+        int l = 0;
+        int m = 0;
+        int h = nums.size()-1;
+
+        while(m <= h){
+            if(nums[m] == 0){
+                swap(nums[l],nums[m]);
+                l++;
+                m++;
             }
-            else if ( nums[i] == 1) {
-                countone++;
+            else if(nums[m] == 1){
+                m++;
             }
-            else if  ( nums[i] == 2) {
-                counttwo++;
+            else{
+                swap(nums[m],nums[h]);
+                h--;
             }
+        } 
 
-        }
 
-        int index = 0;
-        while (countzero-- ) {
-            nums[index] = 0;
 
-            index++ ;
-        }
-        while (countone-- ) {
-            nums[index] = 1;
-
-            index++ ;
-        }
-        while (counttwo-- ) {
-            nums[index] = 2;
-
-            index++ ;
-        }
-
+        
     }
 };
