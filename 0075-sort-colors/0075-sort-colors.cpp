df29da93ebd346128f2317896480b92f9f841314
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-14 15:38:53 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -38,6 +38,14 @@ public:
 
         // Inplace Sorting
 
+        // In-place Sorting (Dutch National Flag Algorithm) TC O(n) SC O(1)
+        // 1. Initialize three pointers: 'l' for 0s (left), 'm' for current element (middle), 'h' for 2s (right).
+        // 2. Iterate while 'm' is less than or equal to 'h'.
+        // 3. If nums[m] is 0, swap nums[l] and nums[m], then increment 'l' and 'm'.
+        // 4. If nums[m] is 1, just increment 'm'.
+        // 5. If nums[m] is 2, swap nums[m] and nums[h], then decrement 'h'.
+        //    (Note: 'm' is not incremented here because the swapped element at 'h' could be 0, 1, or 2 and needs   to be checked.)
+
         int l = 0;
         int m = 0;
         int h = nums.size()-1;
@@ -56,9 +64,6 @@ public:
                 h--;
             }
         } 
-
-
-
         
     }
 };
