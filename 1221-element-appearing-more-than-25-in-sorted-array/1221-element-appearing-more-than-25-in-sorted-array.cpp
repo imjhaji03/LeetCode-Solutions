c96805b353f6ad2d61bcf7a95c96ac9cb93ba75c
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2025-09-20 23:18:43 +0530

    Time: 4 ms (22.72%), Space: 18.1 MB (11.17%) - LeetHub

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, int> mpp;
+
+        for(int i = 0; i < n; i++){
+            mpp[arr[i]]++;
+        }
+
+        for(auto it : mpp){
+            if(it.second > n/ 4){
+                return it.first;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
