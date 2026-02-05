a0fab371893d58c5a9a0bb18d54a4bdf5cc084f6
Amit Gunjan Jha <119215172+imjhaji03@users.noreply.github.com>
2026-02-05 22:21:47 +0530

    Time: 61 ms (49.33%), Space: 19.4 MB (34.53%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -3,16 +3,12 @@ class Solution:
         n = len(nums)
 
         lst = [0]*n
-
         for i in range(n):
             shift = nums[i] % n
             newidx = (i + shift) % n
-
             if(newidx < 0):
                 newidx += n
-
             lst[i] = nums[newidx]
-
 
         return lst        
         
